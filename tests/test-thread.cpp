c93ffb7d60c19ca846772b7943c800c8634e6325
Sebastian Bauer <mail@sebastianbauer.info>
2016-01-22 13:29:55 +0100

    Added simple thread test from Issue #12

---
+++ tests/test-thread.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <thread>
+
+void call_from_thread() {
+	std::cout << "Hello, World" << std::endl;
+}
+
+int main() {
+	std::thread t1(call_from_thread);
+	t1.join();
+	return 0;
+}                   ^
