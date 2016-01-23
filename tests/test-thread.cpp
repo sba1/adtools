5ccba2f278ddb0bca4eecabc83a56968f21c4805
Sebastian Bauer <mail@sebastianbauer.info>
2016-01-23 23:56:45 +0100

    Provide compiling steps for test-thread.
    As the init function of pthread is called too late,
    we provide an own ldscript that ensures that it is
    called first for now.

--- tests/test-thread.cpp
+++ tests/test-thread.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <thread>
 
+#include <proto/dos.h>
+
 void call_from_thread() {
+	IDOS->Delay(100);
 	std::cout << "Hello, World" << std::endl;
+	IDOS->Delay(100);
+	std::cout << "Thread is about to finish" << std::endl;
 }
 
 int main() {
 	std::thread t1(call_from_thread);
+	std::cout << "Start join" << std::endl;
 	t1.join();
+	std::cout << "Join done" << std::endl;
 	return 0;
 }
