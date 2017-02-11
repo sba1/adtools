51911cb9c2e6e9636f97af85c72113933f3d4199
Sebastian Bauer <mail@sebastianbauer.info>
2017-02-11 23:18:23 +0100

    Explain a bit the purpose of travis-gcc-deb.sh

--- travis-gcc-deb.sh
+++ travis-gcc-deb.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
+#
+# Simple shell script that is called in context of travis and that
+# will build the current adtools-gcc packages.
+#
 
 (cd packaging/deb/adtools-gcc && debuild -i -us -uc -b >/tmp/build.out)
