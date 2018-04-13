0bce40d5e8fd46e8a5b2734259713fc71a9f0efb
Sebastian Bauer <mail@sebastianbauer.info>
2018-04-13 19:41:09 +0200

    Made test about aos4 extensions more complete.

--- tests/test-aos4-extensions-simple.c
+++ tests/test-aos4-extensions-simple.c
@@ -4,7 +4,7 @@
  * assembler output or machine code to verify the
  * correctness.
  */
- 
+
 #define va_start(v,l)   __builtin_va_start(v,l)
 #define va_end(v)       __builtin_va_end(v)
 #define va_arg(v,l)     __builtin_va_arg(v,l)
@@ -12,6 +12,7 @@
 #define va_getlinearva(AP,TYPE) ((TYPE)__builtin_getlinearva(AP))
 
 int num;
+int value;
 
 __attribute__((linearvarargs))
 void testvarg_test(int a, int arg, ...)
@@ -26,10 +27,20 @@ void testvarg_test(int a, int arg, ...)
 
 struct Test
 {
-	void * __attribute__((libcall)) (*test)(struct Test*,int);
+	void * __attribute__((libcall)) (*test)(struct Test *,int);
 };
 
-struct Test *test = 0;
+static void *test_function(struct Test *test, int new_value)
+{
+	value = new_value;
+}
+
+struct Test test_instance =
+{
+	test_function
+};
+
+struct Test *test = &test_instance;
 
 void _start(void)
 {
