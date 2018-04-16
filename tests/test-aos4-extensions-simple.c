0a9470571767e409a28881939be6570e329c4c66
Sebastian Bauer <mail@sebastianbauer.info>
2018-04-16 22:30:08 +0200

    Test more features.

--- tests/test-aos4-extensions-simple.c
+++ tests/test-aos4-extensions-simple.c
@@ -7,34 +7,54 @@
 
 #include "common.h"
 
+/******************************************************************************/
+
 #define va_start(v,l)   __builtin_va_start(v,l)
 #define va_end(v)       __builtin_va_end(v)
 #define va_arg(v,l)     __builtin_va_arg(v,l)
 #define va_startlinear(AP,ARG) va_start(AP,ARG)
 #define va_getlinearva(AP,TYPE) ((TYPE)__builtin_getlinearva(AP))
 
-int num;
-int value;
-
 __attribute__((linearvarargs))
-void testvarg_test(int a, int arg, ...)
+void testvarg_test(int count, ...)
 {
-    __builtin_va_list ap;
-    int *ptr;
+	__builtin_va_list ap;
+	unsigned int *ptr;
+	int i;
 
-   va_startlinear(ap, arg);
-   ptr = va_getlinearva(ap, int *);
-   num =*ptr;
+	putuint(count);
+	putchar(' ');
+
+	/* Access the varargs as an array */
+	va_startlinear(ap, count);
+	ptr = va_getlinearva(ap, unsigned int *);
+
+	for (i=0; i < count; i++)
+	{
+		putuint(ptr[i]);
+		putchar(' ');
+	}
+
+	putchar('\n');
 }
+
+/******************************************************************************/
 
 struct Test
 {
-	void * __attribute__((libcall)) (*test)(struct Test *,int);
+	void __attribute__((libcall)) (*test)(struct Test *,int);
 };
 
-static void *test_function(struct Test *test, int new_value)
+
+/* Forward declaration */
+extern struct Test *test;
+
+static void test_function(struct Test *test_, int new_value)
 {
-	value = new_value;
+	putuint(test == test_);
+	putchar(' ');
+	putuint(new_value);
+	putchar('\n');
 }
 
 struct Test test_instance =
@@ -43,6 +63,8 @@ struct Test test_instance =
 };
 
 struct Test *test = &test_instance;
+
+/******************************************************************************/
 
 void _start(void)
 {
