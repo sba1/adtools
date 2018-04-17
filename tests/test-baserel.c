0a22b809c198ca6c410691115fe821e10b50c9d8
Sebastian Bauer <mail@sebastianbauer.info>
2018-04-17 22:13:21 +0200

    Add initial test for working baserel support.
    It doesn't go through yet because the linker no longer understands the
    baserel relocs.

---
+++ tests/test-baserel.c
@@ -0,0 +1,60 @@
+/* A simple test for the so-called baserel features of an AmigaOS4 compiler. */
+
+#include "common.h"
+
+/******************************************************************************/
+
+static int bss[10];
+static int data[10] = {
+	1,2,3,4,5,6,7,8,9,10
+};
+
+/******************************************************************************/
+
+void write_to_global_bss(void)
+{
+	bss[0] = 0x87654321;
+}
+
+__attribute__((baserel_restore))
+void write_to_global_bss_with_restore(void )
+{
+	bss[1] = 0x12345678;
+}
+
+/******************************************************************************/
+
+void write_to_global_data(void)
+{
+	data[0] = 0xDDDDDDDD;
+}
+
+__attribute__((baserel_restore))
+void write_to_global_data_with_restore(void)
+{
+	data[1] = 0xEEEEEEEE;
+}
+
+/******************************************************************************/
+
+void _start(void)
+{
+	init_mmu();
+
+	write_to_global_bss();
+	write_to_global_bss_with_restore();
+
+	write_to_global_data();
+	write_to_global_data_with_restore();
+}
+
+/******************************************************************************/
+
+/* This simulates the data space that is usually allocated dynamically */
+__attribute__((force_no_baserel)) static int all_data[100];
+
+void __baserel_get_addr(void)
+{
+	int *d = all_data;
+	asm volatile ("mr 2, %0" : : "r"(d));
+}
